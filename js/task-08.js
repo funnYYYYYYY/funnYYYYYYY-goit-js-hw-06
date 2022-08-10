b37dbbe7bd751ebe119035c033601aa4c7ebbd9a
Pavlo <pasam9300@gmail.com>
2022-08-10 21:24:49 +0200

    Done 9HM

--- js/task-08.js
+++ js/task-08.js
@@ -5,4 +5,17 @@ form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log(
+      "Всі поля мусять бути заповнені, заповніть їх будь ласка"
+    );
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// const widgetEl = document.querySelector(".widget");
+// console.log(widgetEl);
+const body = document.querySelector("body");
+
+const buttonEl = document.querySelector("button");
+console.log(buttonEl);
+
+const text = document.querySelector(".color");
+
+buttonEl.addEventListener("click", onChangingColorButton);
+
+function onChangingColorButton() {
+  const changeColor = (body.style.backgroundColor = getRandomHexColor());
+  body.style.backgroundColor = getRandomHexColor();
+
+  text.textContent = changeColor;
+}
