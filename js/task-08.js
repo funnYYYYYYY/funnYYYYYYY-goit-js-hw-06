7420fad33ea76a8b7e66b7a33301b0f2f4712072
Pavlo <pasam9300@gmail.com>
2022-08-13 14:43:49 +0200

    Перероблено

--- js/task-08.js
+++ js/task-08.js
@@ -3,39 +3,37 @@ console.log(form);
 
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const formValue = {};
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
-
-  if (email.value === "" || password.value === "") {
-    return alert("Всі поля мусять бути заповнені, заповніть їх будь ласка");
-  }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
-
 // function handleSubmit(event) {
 //   event.preventDefault();
 
-//   const formElements = event.currentTarget.elements;
+//   const formValue = {};
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
 
-//   const email = formElements.email.value;
-//   const password = formElements.password.value;
-
-//   const formValue = {
-//     email,
-//     password,
-//   };
 //   if (email.value === "" || password.value === "") {
 //     return alert("Всі поля мусять бути заповнені, заповніть їх будь ласка");
-//   } else {
-//     formValue;
 //   }
 
-//   console.log(formValue);
+//   console.log(`Email: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
 // }
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formValue = {
+    email,
+    password,
+  };
+  if (email === "" || password === "") {
+    alert("Всі поля мусять бути заповнені, заповніть їх будь ласка");
+  }
+
+  console.log(formValue);
+}
